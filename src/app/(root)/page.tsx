df78e9740776617b6b16ac6806840a4f0e87cdee
Khushil-21 <khushilshah2105@gmail.com>
2025-02-12 12:21:54 +0530

    added clerk authentication

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,11 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 export default function Home() {
-  return (
-    <div>
-      Home
-    </div>
-  )
+	return (
+		<div className="flex flex-col gap-5">
+			Home
+			<UserButton />
+		</div>
+	);
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const IBM_PLEX = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -20,10 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn(IBM_PLEX.variable, "font-ibm-plex antialiased")}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn(IBM_PLEX.variable, "font-ibm-plex antialiased")}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
