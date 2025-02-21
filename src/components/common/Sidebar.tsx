2887f00b5d8bba31e43c416ba44bdf2c6f6f6982
Khushil-21 <khushilshah2105@gmail.com>
2025-02-21 09:58:02 +0530

    SignedOut Sidebar

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,10 +1,11 @@
 "use client"
 import { navLinks } from '@/constants'
-import { SignedIn } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { usePathname } from 'next/navigation'
+import { Button } from '../ui/button'
 
 export default function Sidebar() {
     const pathname = usePathname()
@@ -31,8 +32,20 @@ export default function Sidebar() {
                                         </li>
                                     )
                                 })}
+                            <li className='flex-center cursor-pointer gap-2 p-4'>
+                                <UserButton showName appearance={{
+                                    variables: {
+                                        colorPrimary: "#624cf5",
+                                    },
+                                }} />
+                            </li>
                         </ul>
                     </SignedIn>
+                    <SignedOut>
+                        <Button asChild className='button bg-purple-gradient bg-cover'>
+                            <Link href='/sign-in'>Login</Link>
+                        </Button>
+                    </SignedOut>
                 </nav>
             </div>
         </aside>
