c7b27009a6d094f848e5feb939d4cb26c778e80d
Khushil-21 <khushilshah2105@gmail.com>
2025-03-04 10:58:32 +0530

    added shadcn form

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,16 +1,30 @@
 import Header from '@/components/common/Header';
+import TransformationForm from '@/components/common/TransformationForm';
 import { transformationTypes } from '@/constants';
+import { getUserById } from '@/lib/Database/actions/user.action';
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 export default async function AddTransformationPage({ params }: { params: Promise<{ type: string }> }) {
     const { type } = await params;
+    const { userId } = await auth();
+    if (!userId) redirect('/sign-in');
+
+    const user = await getUserById(userId);
+
     const { title, subTitle } = transformationTypes[type as keyof typeof transformationTypes];
-    console.log("type: ", type);
     return (
         <div>
             <Header
                 title={title}
                 subtitle={subTitle}
+            />
+            <TransformationForm
+                action='Add'
+                userId={user?._id}
+                type={type as TransformationTypeKey}
+                creditBalance={user?.creditBalance}
             />
         </div>
     )
