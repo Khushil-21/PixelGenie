7e37a04f6e57642b55779b3c2fbbe14fabe930d8
Khushil-21 <khushilshah2105@gmail.com>
2025-03-12 11:26:04 +0530

    restore and background removal works , insufficent credits modal

--- src/components/common/TransformationForm.tsx
+++ src/components/common/TransformationForm.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-import React, { useState, useTransition } from "react";
+import React, { useEffect, useState, useTransition } from "react";
 
 import { object, z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -19,6 +19,7 @@ import { Form } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import {
 	aspectRatioOptions,
+	creditFee,
 	defaultValues,
 	transformationTypes,
 } from "@/constants";
@@ -30,6 +31,7 @@ import { updateCredits } from "@/lib/Database/actions/user.action";
 import { getCldImageUrl } from "next-cloudinary";
 import { addImage, updateImage } from "@/lib/Database/actions/image.action";
 import { useRouter } from "next/navigation";
+import { InsufficientCreditsModal } from "./InsufficientCreditsModal";
 
 export const formSchema = z.object({
 	title: z.string(),
@@ -77,69 +79,69 @@ export default function TransformationForm({
 	// 2. Define a submit handler.
 	async function onSubmit(values: z.infer<typeof formSchema>) {
 		setIsSubmitting(true);
-	
-		if(data || image) {
-		  const transformationUrl = getCldImageUrl({
-			width: image?.width,
-			height: image?.height,
-			src: image?.publicId,
-			...transformationConfig
-		  })
-	
-		  const imageData = {
-			title: values.title,
-			publicId: image?.publicId,
-			transformationType: type,
-			width: image?.width,
-			height: image?.height,
-			config: transformationConfig,
-			secureURL: image?.secureURL,
-			transformationURL: transformationUrl,
-			aspectRatio: values.aspectRatio,
-			prompt: values.prompt,
-			color: values.color,
-		  }
-	
-		  if(action === 'Add') {
-			try {
-			  const newImage = await addImage({
-				image: imageData,
-				userId,
-				path: '/'
-			  })
-	
-			  if(newImage) {
-				form.reset()
-				setImage(data)
-				router.push(`/transformations/${newImage._id}`)
-			  }
-			} catch (error) {
-			  console.log(error);
+
+		if (data || image) {
+			const transformationUrl = getCldImageUrl({
+				width: image?.width,
+				height: image?.height,
+				src: image?.publicId,
+				...transformationConfig,
+			});
+
+			const imageData = {
+				title: values.title,
+				publicId: image?.publicId,
+				transformationType: type,
+				width: image?.width,
+				height: image?.height,
+				config: transformationConfig,
+				secureURL: image?.secureURL,
+				transformationURL: transformationUrl,
+				aspectRatio: values.aspectRatio,
+				prompt: values.prompt,
+				color: values.color,
+			};
+
+			if (action === "Add") {
+				try {
+					const newImage = await addImage({
+						image: imageData,
+						userId,
+						path: "/",
+					});
+
+					if (newImage) {
+						form.reset();
+						setImage(data);
+						router.push(`/transformations/${newImage._id}`);
+					}
+				} catch (error) {
+					console.log(error);
+				}
 			}
-		  }
-	
-		  if(action === 'Update') {
-			try {
-			  const updatedImage = await updateImage({
-				image: {
-				  ...imageData,
-				  _id: data._id
-				},
-				userId,
-				path: `/transformations/${data._id}`
-			  })
-	
-			  if(updatedImage) {
-				router.push(`/transformations/${updatedImage._id}`)
-			  }
-			} catch (error) {
-			  console.log(error);
+
+			if (action === "Update") {
+				try {
+					const updatedImage = await updateImage({
+						image: {
+							...imageData,
+							_id: data._id,
+						},
+						userId,
+						path: `/transformations/${data._id}`,
+					});
+
+					if (updatedImage) {
+						router.push(`/transformations/${updatedImage._id}`);
+					}
+				} catch (error) {
+					console.log(error);
+				}
 			}
-		  }
 		}
-	
-		setIsSubmitting(false)
-	  }
+
+		setIsSubmitting(false);
+	}
 
 	function onSelectFieldHandler(
 		value: string,
@@ -193,9 +195,16 @@ export default function TransformationForm({
 		});
 	}
 
+	useEffect(() => {
+		if(image && (type === "restore" || type === "removeBackground")) {
+			setNewTransformation(transformationType.config);
+		}
+	}, [image,transformationType.config,type]);
+
 	return (
 		<Form {...form}>
 			<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+				{creditBalance < Math.abs(creditFee) && <InsufficientCreditsModal />}
 				<CustomField
 					control={form.control}
 					name="title"
