67090ed06132848e39f2a19e51fa3cbe25a7a4b4
Khushil-21 <khushilshah2105@gmail.com>
2025-03-15 15:33:12 +0530

    update page

--- src/app/(root)/transformations/[id]/update/page.tsx
+++ src/app/(root)/transformations/[id]/update/page.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { redirect } from "next/navigation";
+import { transformationTypes } from "@/constants";
+import { auth } from "@clerk/nextjs/server";
+import { getUserById } from "@/lib/Database/actions/user.action";
+import { getImageById } from "@/lib/Database/actions/image.action";
+import TransformationForm from "@/components/common/TransformationForm";
+import Header from "@/components/common/Header";
 
-export default async function UpdateTransformationPage({ params }: { params: Promise<{ id: string }> }) {
-    const { id } = await params;
-    return (
-        <div>Update Transformation Page {id}</div>
-    )
-}
+
+const Page = async ({ params }: SearchParamProps) => {
+    const { userId } = await auth();
+    const {id} = await params;
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  const image = await getImageById(id);
+
+  const transformation =
+    transformationTypes[image.transformationType as TransformationTypeKey];
+
+  return (
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+
+      <section className="mt-10">
+        <TransformationForm
+          action="Update"
+          userId={user._id}
+          type={image.transformationType as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+          config={image.config}
+          data={image}
+        />
+      </section>
+    </>
+  );
+};
+
+export default Page;
