bb313e5da789054523edae64ea9dcc129ab0bcd8
Khushil-21 <khushilshah2105@gmail.com>
2025-03-15 16:41:29 +0530

    Profile Page

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
+import { redirect } from "next/navigation";
 
-export default function ProfilePage() {
+
+import Header from "@/components/common/Header";
+import { getUserImages } from "@/lib/Database/actions/image.action";
+import { getUserById } from "@/lib/Database/actions/user.action";
+import { Collection } from "@/components/common/Collection";
+
+const Profile = async ({ searchParams }: SearchParamProps) => {
+  const paramsData = await searchParams;
+  const page = Number(paramsData?.page) || 1;
+  const { userId } = await auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  const images = await getUserImages({ page, userId: user._id });
+
   return (
-    <div>Profile Page</div>
-  )
-}
+    <>
+      <Header title="Profile" />
+
+      <section className="profile">
+        <div className="profile-balance">
+          <p className="p-14-medium md:p-16-medium">CREDITS AVAILABLE</p>
+          <div className="mt-4 flex items-center gap-4">
+            <Image
+              src="/assets/icons/coins.svg"
+              alt="coins"
+              width={50}
+              height={50}
+              className="size-9 md:size-12"
+            />
+            <h2 className="h2-bold text-dark-600">{user.creditBalance}</h2>
+          </div>
+        </div>
+
+        <div className="profile-image-manipulation">
+          <p className="p-14-medium md:p-16-medium">IMAGE MANIPULATION DONE</p>
+          <div className="mt-4 flex items-center gap-4">
+            <Image
+              src="/assets/icons/photo.svg"
+              alt="coins"
+              width={50}
+              height={50}
+              className="size-9 md:size-12"
+            />
+            <h2 className="h2-bold text-dark-600">{images?.data.length}</h2>
+          </div>
+        </div>
+      </section>
+
+      <section className="mt-8 md:mt-14">
+        <Collection
+          images={images?.data}
+          totalPages={images?.totalPages}
+          page={page}
+        />
+      </section>
+    </>
+  );
+};
+
+export default Profile;
