c7ba6abf7ef95a84d295fef846e63a34dcbafeaf
Khushil-21 <khushilshah2105@gmail.com>
2025-03-20 12:52:37 +0530

    Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,66 +4,81 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const IBM_PLEX = IBM_Plex_Sans({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
-  variable: "--font-ibm-plex",
+	subsets: ["latin"],
+	weight: ["400", "500", "600", "700"],
+	variable: "--font-ibm-plex",
 });
 
 export const metadata: Metadata = {
-  title: "Pixel Genie",
-  description: "AI powered image generator with tools for background removal, image enhancement, object removal, generative fill, and object recoloring",
-  keywords: ["AI image generator", "background removal", "image enhancement", "object removal", "generative fill", "object recoloring"],
-  authors: [{ name: "Pixel Genie" }],
-  creator: "Pixel Genie",
-  publisher: "Pixel Genie",
-  openGraph: {
-    title: "Pixel Genie - AI Image Manipulation Tool",
-    description: "Transform your images with AI-powered tools for background removal, enhancement, object removal, and more",
-    images: [
-      {
-        url: "/assets/images/meta-image.png",
-        width: 1200,
-        height: 630,
-        alt: "Pixel Genie Preview",
-      },
-    ],
-    type: "website",
-    locale: "en_US",
-    siteName: "Pixel Genie",
-    url: "https://pixel-geniee.vercel.app/",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Pixel Genie - AI Image Manipulation Tool",
-    description: "Transform your images with AI-powered tools for background removal, enhancement, object removal, and more",
-    images: ["/assets/images/meta-image.png"],
-    creator: "@pixelgenie",
-  },
-  icons: {
-    icon: ["/favicon.ico"],
-  },
-  metadataBase: new URL("https://pixel-geniee.vercel.app/"),
+	title: "Pixel Genie",
+	description:
+		"AI powered image generator with tools for background removal, image enhancement, object removal, generative fill, and object recoloring",
+	keywords: [
+		"AI image generator",
+		"background removal",
+		"image enhancement",
+		"object removal",
+		"generative fill",
+		"object recoloring",
+	],
+	authors: [{ name: "Pixel Genie" }],
+	creator: "Pixel Genie",
+	publisher: "Pixel Genie",
+	openGraph: {
+		title: "Pixel Genie - AI Image Manipulation Tool",
+		description:
+			"Transform your images with AI-powered tools for background removal, enhancement, object removal, and more",
+		images: [
+			{
+				url: "/assets/images/meta-image.png",
+				width: 1200,
+				height: 630,
+				alt: "Pixel Genie Preview",
+			},
+		],
+		type: "website",
+		locale: "en_US",
+		siteName: "Pixel Genie",
+		url: "https://pixel-geniee.vercel.app/",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "Pixel Genie - AI Image Manipulation Tool",
+		description:
+			"Transform your images with AI-powered tools for background removal, enhancement, object removal, and more",
+		images: ["/assets/images/meta-image.png"],
+		creator: "@pixelgenie",
+	},
+	icons: {
+		icon: ["/favicon.ico"],
+	},
+	metadataBase: new URL("https://pixel-geniee.vercel.app/"),
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <ClerkProvider appearance={{
-      variables: {
-        colorPrimary: "#624cf5",
-      },
-    }}>
-      <html lang="en">
-        <body className={cn(IBM_PLEX.variable, "font-ibm-plex antialiased")}>
-          {children}
-          <Toaster />
-        </body>
-      </html>
-    </ClerkProvider>
-  );
+	return (
+		<ClerkProvider
+			appearance={{
+				variables: {
+					colorPrimary: "#624cf5",
+				},
+			}}
+		>
+			<html lang="en">
+				<body className={cn(IBM_PLEX.variable, "font-ibm-plex antialiased")}>
+					{children}
+					<Toaster />
+				</body>
+				<Analytics />
+				<SpeedInsights />
+			</html>
+		</ClerkProvider>
+	);
 }
